c0ffea1bdf51a322653c3f4577de1ff106159536
JoYoungWook 조영욱 <siosio24@naver.com>
2021-07-06 18:53:28 +0900

    lombok install

---
+++ src/test/java/com/springboot/st/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.springboot.st;
+
+import com.springboot.st.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello_return() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string(hello));
+    }
+}
