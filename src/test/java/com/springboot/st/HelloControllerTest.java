4281b18f702e757c8dc2a8bb343f712e1c608e3a
JoYoungWook 조영욱 <siosio24@naver.com>
2021-07-07 19:46:55 +0900

    dto test

--- src/test/java/com/springboot/st/HelloControllerTest.java
+++ src/test/java/com/springboot/st/HelloControllerTest.java
@@ -1,14 +1,16 @@
 package com.springboot.st;
 
 import com.springboot.st.web.HelloController;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -21,6 +23,18 @@ public class HelloControllerTest {
     public void hello_return() throws Exception {
         String hello = "hello";
 
-        mvc.perform(MockMvcRequestBuilders.get("/hello")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string(hello));
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
     }
+
+    @Test
+    public void hello_Return_Dto() throws Exception{
+        String name= "hello";
+        int amount = 1000;
+        mvc.perform(get("/hello/dto").param("name",name)
+        .param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", Matchers.is(name)))
+                .andExpect(jsonPath("$.amount",Matchers.is(amount)));
+    }
+
 }
