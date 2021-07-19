fb0a5fed3b03554437ff519bcffcf68b9101e925
JoYoungWook 조영욱 <siosio24@naver.com>
2021-07-19 20:52:11 +0900

    CRUD

--- src/main/java/com/springboot/st/service/posts/PostsService.java
+++ src/main/java/com/springboot/st/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.springboot.st.service.posts;
 
 import com.springboot.st.domain.posts.Posts;
 import com.springboot.st.domain.posts.PostsRepository;
+import com.springboot.st.web.dto.PostsListResponseDto;
 import com.springboot.st.web.dto.PostsResponseDto;
 import com.springboot.st.web.dto.PostsSaveRequestDto;
 import com.springboot.st.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,10 +32,25 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).
+                orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다 id="+ id));
+        postsRepository.delete(posts);
+    }
+
     //어떤 값을 받아올지 db와 통신하여 그 값을 리턴
     public PostsResponseDto findById(Long id){
         Posts entity= postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다" + id));
         return  new PostsResponseDto(entity);
+    }
+
+
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 
 
