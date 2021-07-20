0487a459f9cbc28c7cb2b8d1fbda27d77e2406f9
JoYoungWook 조영욱 <siosio24@naver.com>
2021-07-20 21:04:49 +0900

    Naver Login

--- src/main/java/com/springboot/st/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/springboot/st/config/auth/dto/OAuthAttributes.java
@@ -25,16 +25,31 @@ public class OAuthAttributes {
     }
 
     //OAuth2User에서 반환하는 사용자 값은 Map이기 때문에 하나하나 변환 해야합니다
-    public static OAuthAttributes of(String registrationId,String userNameAttributeName, Map<String,Object> attributes){
-        return ofGoogle(userNameAttributeName,attributes);
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+        return ofGoogle(userNameAttributeName, attributes);
     }
 
-    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String,Object> attributes){
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
-                .name((String)attributes.get("name"))
-                .email((String)attributes.get("email"))
-                .picture((String)attributes.get("picture"))
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
@@ -42,7 +57,7 @@ public class OAuthAttributes {
     //User엔티티를 생성합니다
     // 엔티티를 생성하는 시점은 처음 가입 할 때입니다
     //가입 때 기본권한을 GUEST로 주기위해서 role빌더값에는 GUEST를 사용합니다
-    public User toEntity(){
+    public User toEntity() {
         return User.builder()
                 .name(name)
                 .email(email)
