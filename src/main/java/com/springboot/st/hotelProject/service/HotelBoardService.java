9815e322e8d57fde89c9a90671d61513ed0aeebf
JoYoungWook 조영욱 <siosio24@naver.com>
2021-12-21 16:56:05 +0900

    Board Service //  S3 upload Refactoring // create delete

--- src/main/java/com/springboot/st/hotelProject/service/HotelBoardService.java
+++ src/main/java/com/springboot/st/hotelProject/service/HotelBoardService.java
@@ -20,17 +20,17 @@ public class HotelBoardService {
     private final Hotel_BoardRepository hotel_boardRepository;
 
     public Hotel_Board save(MultipartFile multipartFile,
-                            HashMap<String, Object> params) {
+                            String title, String content) {
 
         Map<String, String> imgPath = s3service.serverSaveMap(multipartFile);
 
         Hotel_Board hotel_board =
                 new Hotel_Board().builder().
-                        content((String)params.get("content")).
+                        content(content).
                         imgPath(imgPath.get("serverURL")).
                         imgOriginName(imgPath.get("originName")).
                         imgUuid(imgPath.get("uuid")).
-                        title((String)params.get("title")).
+                        title(title).
                                 build();
 
         hotel_boardRepository.save(hotel_board);
